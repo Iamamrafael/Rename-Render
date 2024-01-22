46e1c9e9719ca58579d8b3dc3ac0bfe98c77419a
Iamamrafael <157337256+Iamamrafael@users.noreply.github.com>
2024-01-22 16:43:27 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
+# Don't Remove Credit @iamraphaelleo 
 # Subscribe YouTube Channel For Amazing Bot @iamraphaelleo
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @iamraphaelleo 
 
 
 import re, os
@@ -13,13 +13,13 @@ API_HASH = os.environ.get("API_HASH", "95c8ab0714c6bb6742ce2a9ee5fd8ad8")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6557223577:AAFomPQOd40HExjZ210WaHK4eMCtPqvYxxs") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "iamraphaelleo") 
 
              # Don't Remove Credit @iamraphaelleo
              # Subscribe YouTube Channel For Amazing Bot @iamraphaelleo
              # Ask Doubt on telegram @iamraphaelleo
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "renameIamamrafael")     
 
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://akintorlaamrafael01:wQeIXwUD1ZNVMxdq@cluster0.3zwmr8s.mongodb.net/?retryWrites=true&w=majority")
  
